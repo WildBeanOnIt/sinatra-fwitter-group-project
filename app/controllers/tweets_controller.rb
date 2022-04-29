45d097cdb76d70c23028e874e486f9e4061af33e
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-04-28 21:15:43 -0500

    done with login and logout

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,16 @@
 class TweetsController < ApplicationController
 
+    get "/tweets" do
+        if logged_in?
+            @tweets = Tweet.all
+            erb :"tweets/tweets"
+        else
+            redirect to "/login"
+        end
+    end
+
+
+
+
 
 end
